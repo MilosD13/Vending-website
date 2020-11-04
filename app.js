0d0e8ae751db97f25e387ad533d13c156df421c5
Milos Dokic <milosdokic13@gmail.com>
2020-11-04 15:06:05 +1100

    initial setup

---
+++ app.js
@@ -0,0 +1,119 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+const navLogo = document.querySelector("#navbar__logo");
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+};
+
+menu.addEventListener("click", mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+  const elem = document.querySelector(".highlight");
+  const homeMenu = document.querySelector("#home-page");
+  const aboutMenu = document.querySelector("#about-page");
+  const servicesMenu = document.querySelector("#services-page");
+  let scrollPos = window.scrollY;
+  // console.log(scrollPos);
+
+  // adds 'highlight' class to my menu items
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add("highlight");
+    homeMenu.classList.remove("highlight");
+    servicesMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    servicesMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  }
+
+  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove("highlight");
+  }
+};
+
+window.addEventListener("scroll", highlightMenu);
+window.addEventListener("click", highlightMenu);
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector(".is-active");
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.remove("active");
+  }
+};
+
+menuLinks.addEventListener("click", hideMobileMenu);
+navLogo.addEventListener("click", hideMobileMenu);
+
+/* SLIDER */
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+const auto = false; // Auto scroll
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  // Get current class
+  const current = document.querySelector(".current");
+  // Remove current class
+  current.classList.remove("current");
+  // Check for next slide
+  if (current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add("current");
+  } else {
+    // Add current to start
+    slides[0].classList.add("current");
+  }
+  setTimeout(() => current.classList.remove("current"));
+};
+
+const prevSlide = () => {
+  // Get current class
+  const current = document.querySelector(".current");
+  // Remove current class
+  current.classList.remove("current");
+  // Check for prev slide
+  if (current.previousElementSibling) {
+    // Add current to prev sibling
+    current.previousElementSibling.classList.add("current");
+  } else {
+    // Add current to last
+    slides[slides.length - 1].classList.add("current");
+  }
+  setTimeout(() => current.classList.remove("current"));
+};
+
+// Button events
+next.addEventListener("click", (e) => {
+  nextSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+prev.addEventListener("click", (e) => {
+  prevSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+// Auto slide
+if (auto) {
+  // Run next slide at interval time
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
